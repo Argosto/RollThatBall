5d5e2e6c3d981dfbd9a24d6a073f77ca3c765a7c
Argosto <thecanadianpork@gmail.com>
2017-02-02 13:22:24 -0700

    UnityBall

---
+++ isgud/Assets/Scripts/Particles.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particles : MonoBehaviour {
+    public GameObject player;
+    public float lengthmodifier = 1.5f;
+    public float lifeTime = 0.1f;
+
+    private ParticleSystem ps;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position = player.transform.position;
+        ps = gameObject.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+        transform.rotation = player.transform.rotation;
+	}
+
+    public void Upgrade()
+    {
+        lifeTime = lifeTime * lengthmodifier;
+        ps.startLifetime = lifeTime;
+    }
+}
---
+++ isgud/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int count;
+
+    public Text countText;
+    public float speed;
+    public Text winText;
+    public Particles trail;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVeritcal = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVeritcal);
+
+        rb.AddForce(movement * speed);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            trail.Upgrade();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count; " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "WINNER IS YOU!";
+        }
+    }
+}
